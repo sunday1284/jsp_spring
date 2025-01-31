219d098fdd21984c636337fb03d368546d3236df
Your Name <sunday1284>
2025-01-31 16:01:55 +0900

    study2_0131

---
+++ WebStudy02_Mybatis/src/main/java/kr/or/ddit/IndexControllerServlet.java
@@ -0,0 +1,18 @@
+package kr.or.ddit;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/index.do")
+public class IndexControllerServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = "/WEB-INF/views/index.jsp";
+		req.getRequestDispatcher(path).forward(req, resp);
+	}
+}
