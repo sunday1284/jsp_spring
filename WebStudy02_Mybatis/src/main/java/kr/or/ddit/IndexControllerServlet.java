cd7a7985e377678315d7781b6d34fd1d25284255
Your Name <sunday1284>
2025-01-31 18:09:55 +0900

    study02_0131

--- WebStudy02_Mybatis/src/main/java/kr/or/ddit/IndexControllerServlet.java
+++ WebStudy02_Mybatis/src/main/java/kr/or/ddit/IndexControllerServlet.java
@@ -2,17 +2,26 @@ package kr.or.ddit;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 
 @WebServlet("/index.do")
 public class IndexControllerServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String path = "/WEB-INF/views/index.jsp";
-		req.getRequestDispatcher(path).forward(req, resp);
+		String logicalName = "index";
+		
+		if (logicalName.startsWith("redirect:")) {
+			String location = logicalName.replace("redirect:", req.getContextPath());
+			resp.sendRedirect(location);
+		} else {
+			String path = "/" + logicalName + ".tiles";
+			req.getRequestDispatcher(path).forward(req, resp);
+		}
 	}
 }
--- WebStudy02_Mybatis/src/main/webapp/WEB-INF/views/includee/title.jsp
+++ WebStudy02_Mybatis/src/main/webapp/WEB-INF/views/includee/title.jsp
@@ -1,4 +1,4 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-
-웰컴페이지
+	
+<h4>모든 컨텐츠에서 사용할 공통 제목</h4>
